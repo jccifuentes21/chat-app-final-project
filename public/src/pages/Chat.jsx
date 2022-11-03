aec544450b4e65347fbda5dd8d40a1bc7b7b731e
DaweUrbi <urban.da@icloud.com>
2022-11-03 10:40:03 -0700

    contact section done

--- public/src/pages/Chat.jsx
+++ public/src/pages/Chat.jsx
@@ -3,63 +3,75 @@ import styled from "styled-components";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
 import { allUsersRoute } from "../utils/APIRoutes";
+import Contacts from "../components/Contacts";
 
 const Chat = () => {
-  const navigate = useNavigate();
-  const [contacts, setContacts] = useState([]);
-  const [currentUser, setCurrentUser] = useState(undefined);
+	const navigate = useNavigate();
+	const [contacts, setContacts] = useState([]);
+	const [currentUser, setCurrentUser] = useState(undefined);
+	const [currentChat, setCurrentChat] = useState(undefined);
 
-  useEffect(() => {
-    const checkUser = async () => {
-      if (!localStorage.getItem("chat-app-user")) {
-        navigate("/login");
-      } else {
-        setCurrentUser(await JSON.parse(localStorage.getItem("chat-app-user")));
-      }
-    };
-    checkUser();
-  }, []);
+	useEffect(() => {
+		const checkUser = async () => {
+			if (!localStorage.getItem("chat-app-user")) {
+				navigate("/login");
+			} else {
+				setCurrentUser(await JSON.parse(localStorage.getItem("chat-app-user")));
+			}
+		};
+		checkUser();
+	}, []);
 
-  useEffect(() => {
-    const fetchContacts = async () => {
-      if (currentUser) {
-        if (currentUser.isAvatarImageSet) {
-          const data = await axios.get(`${allUsersRoute}/${currentUser._id}`);
-          setContacts(data.data);
-        } else {
-          navigate(`/setAvatar`);
-        }
-      }
-    };
+	useEffect(() => {
+		const fetchContacts = async () => {
+			if (currentUser) {
+				if (currentUser.isAvatarImageSet) {
+					const data = await axios.get(`${allUsersRoute}/${currentUser._id}`);
+					setContacts(data.data);
+				} else {
+					navigate(`/setAvatar`);
+				}
+			}
+		};
 		fetchContacts();
-  }, [currentUser]);
+	}, [currentUser]);
 
-  return (
-    <Container>
-      <div className="container"></div>
-    </Container>
-  );
+	const handleChatChange = (chat) => {
+		setCurrentChat(chat);
+	};
+
+	return (
+		<Container>
+			<div className="container">
+				<Contacts
+					contacts={contacts}
+					currentUser={currentUser}
+					changeChat={handleChatChange}
+				/>
+			</div>
+		</Container>
+	);
 };
 
 const Container = styled.div`
-  height: 100vh;
-  width: 100vw;
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  gap: 1rem;
-  align-items: center;
-  background-color: var(--background-color);
-  .container {
-    height: 85vh;
-    width: 85vw;
-    background-color: var(--form-background-color);
-    display: grid;
-    grid-template-columns: 25% 75%;
-    @media screen and (max-width: 720px) and (min-width: 1080px) {
-      grid-template-columns: 35% 65%;
-    }
-  }
+	height: 100vh;
+	width: 100vw;
+	display: flex;
+	flex-direction: column;
+	justify-content: center;
+	gap: 1rem;
+	align-items: center;
+	background-color: var(--background-color);
+	.container {
+		height: 85vh;
+		width: 85vw;
+		background-color: var(--form-background-color);
+		display: grid;
+		grid-template-columns: 25% 75%;
+		@media screen and (max-width: 720px) and (min-width: 1080px) {
+			grid-template-columns: 35% 65%;
+		}
+	}
 `;
 
 export default Chat;
