9c71c34ca90e83a1127fec36e299d66c804c93d7
DaweUrbi <urban.da@icloud.com>
2022-11-16 09:05:03 -0800

    add logout btn to welcome page

--- public/src/components/Welcome.jsx
+++ public/src/components/Welcome.jsx
@@ -1,44 +1,61 @@
 import React, { useEffect, useState } from "react";
 import styled from "styled-components";
 import welcome from "../assets/welcome.gif";
+import Logout from "./Logout";
 
 const Welcome = () => {
-  const [user, setUser] = useState(undefined);
-  useEffect(() => {
-    const getUser = async () => {
-      setUser(await JSON.parse(localStorage.getItem("chat-app-user")));
-    };
-    getUser();
-  }, []);
+	const [user, setUser] = useState(undefined);
+	useEffect(() => {
+		const getUser = async () => {
+			setUser(await JSON.parse(localStorage.getItem("chat-app-user")));
+		};
+		getUser();
+	}, []);
 
-  return (
-    <Container>
-      <img src={welcome} alt="Welcome gif" />
-      {user ? (
-        <h1>
-          Welcome, <span>{user.username}!</span>
-        </h1>
-      ) : (
-        <h1>Welcome!</h1>
-      )}
-      <h3>Please select a chat to start messaging</h3>
-    </Container>
-  );
+	return (
+		<Container>
+			<div className="welcome-header">
+				<Logout />
+			</div>
+			<div className="welcome-body">
+				<img src={welcome} alt="Welcome gif" />
+				{user ? (
+					<h1>
+						Welcome, <span>{user.username}!</span>
+					</h1>
+				) : (
+					<h1>Welcome!</h1>
+				)}
+				<h3>Please select a chat to start messaging</h3>
+			</div>
+		</Container>
+	);
 };
 
 const Container = styled.div`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  flex-direction: column;
-  color: var(--primary-text-color);
+	color: var(--primary-text-color);
+	.welcome-body {
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		flex-direction: column;
+    height: 80%;
 
-  span {
-    color: var(--secondary-color);
-  }
-  img{
-    height: 20rem;
-  }
+	}
+	.welcome-header {
+		display: flex;
+		justify-content: end;
+		align-items: center;
+		padding: 0 2rem;
+		height: 5rem;
+	}
+
+	span {
+		color: var(--secondary-color);
+	}
+	img {
+		height: 30rem;
+	}
 `;
 
 export default Welcome;
