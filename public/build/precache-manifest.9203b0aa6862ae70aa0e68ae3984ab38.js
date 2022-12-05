73c5ed53224d6859685f2079941f49c188304ab9
DaweUrbi <urban.da@icloud.com>
2022-12-04 19:26:36 -0800

    npm run build add

---
+++ public/build/precache-manifest.9203b0aa6862ae70aa0e68ae3984ab38.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "ec2f533ff2bcf1e3e221",
+    "url": "/static/css/main.17e561a5.chunk.css"
+  },
+  {
+    "revision": "ec2f533ff2bcf1e3e221",
+    "url": "/static/js/main.ec2f533f.chunk.js"
+  },
+  {
+    "revision": "5dc7daa3024dd24b3c69",
+    "url": "/static/css/1.cbc8fc6c.chunk.css"
+  },
+  {
+    "revision": "5dc7daa3024dd24b3c69",
+    "url": "/static/js/1.5dc7daa3.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "c92c99b65392dfa8317be3218dce4eb6",
+    "url": "/static/media/welcome.c92c99b6.gif"
+  },
+  {
+    "revision": "466bbd979ce51ca008dec748d90a59f2",
+    "url": "/static/media/loader.466bbd97.gif"
+  },
+  {
+    "revision": "f146af3c00b8f5d63cb93ca5b0b6fde4",
+    "url": "/index.html"
+  }
+];
